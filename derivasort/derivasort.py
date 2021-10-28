ffd577686853d03b41725e5bdcabb43d38620bff
AdrtH <90867832+AdrtH@users.noreply.github.com>
2021-10-28 19:54:21 +0200

    Implemantation de split list

--- derivasort/derivasort.py
+++ derivasort/derivasort.py
@@ -1,27 +1,39 @@
 from random import randint
+from sys import *
 
 randomListe = [randint(0,99) for i in range(10)]
 
 testList = [1,2,8,6,54,4,3]
 
 def derivasort(liste:list) -> list:
-    listVariation = derive(liste) #liste ayant, à la place des elements, la différence entre 2 éléments
-    for i in range(len(listVariation)):
-        jsp
+    splitLists = splitList(liste, derive(liste))
+    
+    
+
+def splitList(liste:list, sequences:list) -> list:
+    count = 0
+    liste.reverse()
+    newLists: list[list] = [[]] #lists des listes
+    for i in range(len(sequences)):
+        if sequences[i] >= 0:
+            newLists[count].append(liste.pop())
+        if sequences[i] < 0:
+            newLists.append([])
+            count += 1
+    return newLists
 
 
-
-def derive(liste:list) -> list:
+def derive(liste:list) -> list: #retourne une liste avec le sens de variation entre i et i+1
     newListe = []
     for i in range(len(liste) -1):
         newListe.append(liste[i+1] - liste[i])
     if newListe == []: newListe = [0]
     return newListe
 
-def deriveToInfinity(liste:list) -> None:
+def deriveToInfinity(liste:list) -> None: #cette fonction sert a rien mais elle est drole
     if len(liste) <= 1:
         return liste
     print(derive(liste))
     print(deriveToInfinity(derive(liste)))
 
-print(derivasort())
+print()
